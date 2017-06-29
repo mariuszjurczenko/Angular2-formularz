3b77132673800f80ffae77e4dd3846ff7edfbfe5
mariusz jurczenko <mariuszjurczenko@gmail.com>
2017-06-29 10:18:51 +0200

    rejetracja krok 1

--- src/signup-form.component.ts
+++ src/signup-form.component.ts
@@ -3,9 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'signup-form',
   template: `
-    do zrobienia...
+    <form (submit)="onSubmit()">
+      <div class="form-group">
+        <label>Email</label>
+        <input type="email" class="form-control" [(ngModel)]="email" name="email" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Rejestracja</button>
+    </form>
   `
 })
 export class SignupFormComponent {
 
+  email = '';
+
+  onSubmit(){
+    console.log('Test onSubmit', this.email);
+  }
 }
