054a70a2e86844a2304a09ca1a0d08490a3d6cbe
mariusz jurczenko <mariuszjurczenko@gmail.com>
2017-08-26 09:24:50 +0200

    odc39

--- src/signup-form.component.ts
+++ src/signup-form.component.ts
@@ -3,14 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'signup-form',
   template: `
-    <form (submit)="onSubmit()">
+    <form (submit)="onSubmit()" novalidate>
       <div class="form-group">
         <label>Email</label>
-        <input type="email" class="form-control" [(ngModel)]="email" name="email" required>
+        <input type="email" class="form-control" [(ngModel)]="email" name="email" 
+               required pattern=".+@.+">
       </div>
       <button type="submit" class="btn btn-primary">Rejestracja</button>
     </form>
-  `
+  `,
+  styles:[`
+        input.ng-touched.ng-invalid { border:solid red 2px}
+        input.ng-touched.ng-valid { border:solid green 2px}
+    `]
 })
 export class SignupFormComponent {
 
