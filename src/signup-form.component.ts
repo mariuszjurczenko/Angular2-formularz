b93c6354bd10518e45c25b1f514181f0dfc9d5dc
mariusz jurczenko <mariuszjurczenko@gmail.com>
2017-09-06 06:48:55 +0200

    odc41

--- src/signup-form.component.ts
+++ src/signup-form.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'signup-form',
   template: `
-    <form (submit)="onSubmit()" novalidate>
+    <form (submit)="onSubmit(form)" novalidate #form="ngForm">
       <div class="form-group">
         <label>Email</label>
         <input type="email" class="form-control" [(ngModel)]="email" name="email" #emailField="ngModel"
@@ -13,7 +13,16 @@ import { Component } from '@angular/core';
         <p *ngIf="emailField.touched && emailField.invalid && emailField.errors.pattern" 
             class="alert alert-danger">Adres email jest nieprawidłowy</p>
       </div>
-      <button type="submit" class="btn btn-primary">Rejestracja</button>
+
+       <div class="form-group">
+        <label>Hasło</label>
+        <input type="password" class="form-control" [(ngModel)]="password" name="password" #passwordField="ngModel"
+               required>
+        <p *ngIf="passwordField.touched && passwordField.invalid" 
+            class="alert alert-danger">Wymagany jest hasło</p>
+      </div>
+
+      <button type="submit" class="btn btn-primary" [disabled]="form.invalid">Rejestracja</button>
     </form>
   `,
   styles:[`
@@ -24,8 +33,10 @@ import { Component } from '@angular/core';
 export class SignupFormComponent {
 
   email = '';
+  password = '';
 
-  onSubmit(){
+  onSubmit(form){
     console.log('Test onSubmit', this.email);
+    form.resetForm();
   }
 }
